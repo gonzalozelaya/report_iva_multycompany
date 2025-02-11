127c65c98c1fe958cd3ef2224c06792ce40857fa
Gonzalo Zelaya <gonzalo.zelaya@outsourcearg.com>
2025-02-11 09:43:00 -0300

    Updated manifest

--- __manifest__.py
+++ __manifest__.py
@@ -1,35 +1,24 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "my_module",
+    'name': "fix_iva_multicompany",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        This module fix the IVA report for multicompany""",
 
     'description': """
-        Long description of module's purpose
+        This module fix the IVA report for multicompany 
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Outsourcearg",
+    'website': "https://www.outsourcearg.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '1.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['l10n_ar_reports'],
 
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
 }
